a75d63befa82cf39bdd9a36f76137c190d2c0312
camille chaize <c.chaize74@gmail.com>
2024-05-06 13:40:56 +0200

    add graphicswindow class

---
+++ graphicswindow.cpp
@@ -0,0 +1,6 @@
+#include "graphicswindow.h"
+
+graphicsWindow::graphicsWindow()
+{
+
+}
---
+++ graphicswindow.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHICSWINDOW_H
+#define GRAPHICSWINDOW_H
+
+
+class graphicsWindow
+{
+public:
+    graphicsWindow();
+};
+
+#endif // GRAPHICSWINDOW_H
